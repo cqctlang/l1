c23a37613d5ae23fa42c44ab6e8bd756c0a8731d
vic zandy <vic@or>
2009-01-28 01:12:34 -0500

    openssl portability git tweaks

--- fnsec.c
+++ fnsec.c
@@ -1,8 +1,9 @@
 #include "sys.h"
 #include "util.h"
 #include "syscqct.h"
-#include <openssl/sha.h>
+#include <openssl/evp.h>
 
+#if 1
 static void
 l1_sha1(VM *vm, Imm argc, Val *argv, Val *rv)
 {
@@ -25,6 +26,36 @@ l1_sha1(VM *vm, Imm argc, Val *argv, Val *rv)
 	}
 	*rv = mkvalstr(mkstr((char*)omd, 2*SHA_DIGEST_LENGTH));
 }
+#endif
+
+#if 0
+static void
+l1_sha1(VM *vm, Imm argc, Val *argv, Val *rv)
+{
+	Str *s;
+        EVP_MD_CTX mdctx;
+	unsigned char md[SHA_DIGEST_LENGTH];
+	unsigned char omd[2*SHA_DIGEST_LENGTH];
+	unsigned i;
+	static char x[]="0123456780abcdef";
+
+	if(argc != 1)
+		vmerr(vm, "wrong number of arguments to sha1");
+	checkarg(vm, "sha1", argv, 0, Qstr);
+
+        EVP_MD_CTX_init(&mdctx);
+        EVP_DigestInit_ex(&mdctx, EVP_sha1, 0);
+        EVP_DigestUpdate(&mdctx, s->s, (unsigned int)s->len);
+        EVP_DigestFinal_ex(&mdctx, md, 0);
+        EVP_MD_CTX_cleanup(&mdctx);
+
+	for(i = 0; i < SHA_DIGEST_LENGTH; i++){
+		omd[i*2] = x[md[i]>>4];
+		omd[i*2+1] = x[md[i]&0xf];
+	}
+	*rv = mkvalstr(mkstr((char*)omd, 2*SHA_DIGEST_LENGTH));
+}
+#endif
 
 void
 fnsec(Env *env)
