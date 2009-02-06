c8bd7d143d3696f8bbf6607e261f0b6b0604625f
vic zandy <vic@or>
2009-02-06 00:26:59 -0500

    libmp and libsec

---
+++ x/libmp/os.h
@@ -0,0 +1,12 @@
+#include "sys.h"
+#include "util.h"
+
+int dec16(uchar *out, int lim, char *in, int n);
+int enc16(char *out, int lim, uchar *in, int n);
+int dec32(uchar *dest, int ndest, char *src, int nsrc);
+int enc32(char *dest, int ndest, uchar *src, int nsrc);
+int dec64(uchar *out, int lim, char *in, int n);
+int enc64(char *out, int lim, uchar *in, int n);
+
+
+
