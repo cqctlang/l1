7d63a0b587f24c5ab5ed522aaef83d817f48f7ae
vic zandy <zandy@cs.wisc.edu>
2009-07-09 14:13:22 -0400

    subtype: accept (and chase) typedef'd arguments

--- vm.c
+++ vm.c
@@ -7341,7 +7341,7 @@ l1_subtype(VM *vm, Imm argc, Val *argv, Val *rv)
 	if(argv[0]->qkind != Qxtn)
 		vmerr(vm, err);
 		      
-	xtn = valxtn(argv[0]);
+	xtn = chasetype(valxtn(argv[0]));
 	if(xtn->tkind != Tptr && xtn->tkind != Tarr
 	   && xtn->tkind != Tenum && xtn->tkind != Tconst
 	   && xtn->tkind != Txaccess && xtn->tkind != Tundef)
