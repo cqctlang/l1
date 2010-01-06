5580846c86ad65f5c8264a49ec397400029c219c
vic zandy <zandy@cs.wisc.edu>
2010-01-06 18:35:35 -0500

    demo/mclones: new demo

---
+++ demo/mclones.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+
+enum
+{
+	N = 5,
+	M = 100,
+};
+long depth;
+
+
+static void*
+xmalloc(int sz)
+{
+	return malloc(sz);
+}
+
+static void
+xfree(void *p)
+{
+	free(p);
+}
+
+static void
+work()
+{
+	int i;
+	void *p[M];
+	for(i = 0; i < M; i++)
+		p[i] = xmalloc(i);
+	for(i = 0; i < M; i++)
+		xfree(p[i]);
+}
+
+static void*
+doit(void *p)
+{
+	pthread_t t[N];
+	unsigned long i;
+	long n;
+
+	n = (long)p;
+	printf("clone level %ld\n", n);
+	work();
+	if(n == 0)
+		return 0;
+	for(i = 0; i < N; i++)
+		pthread_create(&t[i], 0, doit, (void*)(n-1));
+	for(i = 0; i < N; i++)
+		pthread_join(t[i], 0);
+	return 0;
+}
+
+int
+main(int argc, char *argv[])
+{
+	pthread_t t;
+	if(argc > 1)
+		depth = (long)atoi(argv[1]);
+	else
+		depth = 1;
+	pthread_create(&t, 0, doit, (void*)depth);
+	pthread_join(t, 0);
+	return 0;
+}
