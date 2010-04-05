99ff0090fbd8113b923923739c55112c5a453e00
vic zandy <vic@or>
2010-04-05 10:55:56 -0400

    compilef: hacking

--- compilef.c
+++ compilef.c
@@ -77,11 +77,62 @@ rmuseless(U *ctx, Expr *e)
 	return e;
 }
 
+static Expr*
+rmgoto(U *ctx, Expr *e, Expr **bs)
+{
+	if(e == 0)
+		return 0;
+	switch(e->kind){
+	case Elabel:
+		id = e->id;
+		e->e1 = rm(ctx, e->e1, ls);
+		if(hget(ls, id, strlen(id)))
+			return e;
+		se = e->e1;
+		e->e1 = 0;
+		freeexpr(e);
+		return se;
+	case Egoto:
+	case Eelist:
+		p = e;
+		while(p->kind == Eelist){
+			p->e1 = rmgoto(ctx, p->e1, ls);
+			p = p->e2;
+		}
+		return e;
+	default:
+		e->e1 = rmgoto(ctx, e->e1, ls);
+		e->e2 = rmgoto(ctx, e->e2, ls);
+		e->e3 = rmgoto(ctx, e->e3, ls);
+		e->e4 = rmgoto(ctx, e->e4, ls);
+		return e;
+	}
+}
+
+static Expr*
+rmgoto0(U *ctx, Expr *e)
+{
+	Expr *p, *q, **bs;
+
+	if(e->kind != Eletrec)
+		fatal("bug");
+	bs = &e->e1;
+	p = e->e1;
+	while(p->kind == Eelist){
+		q = p->e1;
+		q->e2->e1 = rmgoto(ctx, q->e2->e1, bs);
+		p = p->e2;
+	}
+	e->e2 = rmgoto(ctx, e->e2, bs);
+	return e;
+}
+
 Expr*
 docompilef(U *ctx, Expr *e)
 {
 	if(setjmp(ctx->jmp) != 0)
 		return 0;	/* error */
 	e = rmuseless(ctx, e);
+	e = rmgoto0(ctx, e);
 	return e;
 }
