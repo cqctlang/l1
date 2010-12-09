476f5e7d50cc4a601bba64dd92e1f599b5312809
vic zandy <zandy@cs.wisc.edu>
2010-12-09 17:58:40 -0500

    vm.c: new cheery FIXME about lurking gc bug

--- vm.c
+++ vm.c
@@ -4608,6 +4608,11 @@ dogc(VM *vm, u32 g, u32 tg)
 	_gc(g, tg);
 	v = cqctenvlook(vm->top, "postgc");
 	if(v && Vkind(v) == Qcl){
+		/* FIXME: this is almost certainly
+		   broken, as least spirtually; the gc
+		   does not know to scan this object
+		   since it does not appear in any
+		   mask. */
 		vmpush(vm, vm->ac);
 		dovm(vm, valcl(v), 0, 0);
 		vm->ac = vm->stack[vm->sp];
