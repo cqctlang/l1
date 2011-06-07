cd02620b1d7c647c849bbf8019ea7781f9812a4a
vic zandy <zandy@cs.wisc.edu>
2011-06-07 16:05:46 -0400

    errno: fix bug

--- fnsys.c
+++ fnsys.c
@@ -364,9 +364,9 @@ l1_errno(VM *vm, Imm argc, Val *argv, Val *rv)
 	l = mklistinit(2, Xnil);
 	*rv = mkvallist(l);
 	no = getlasterrno();
+	listset(vm, l, 0, mkvalcval2(mklitcval(Vint, no)));
 	if(no == 0)
 		return;
-	listset(vm, l, 0, mkvalcval2(mklitcval(Vint, no)));
 	listset(vm, l, 1, mkvalstr(mkstr0(strerror(errno))));
 }
 
