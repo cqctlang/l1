f6ee279422c98682fb2e3c763b327a69539f9727
vic zandy <zandy@cs.wisc.edu>
2012-09-23 09:54:38 -0400

    cpx: construct #id and #val forms correctly

--- cpx.c
+++ cpx.c
@@ -3,42 +3,46 @@
 #include "syscqct.h"
 
 static Expr*
-stx(Expr *e)
+stx(U *ctx, Expr *e)
 {
 	Expr *p;
 	char *id;
 
 	if(e == 0)
-		return 0; 
+		return 0;
 	switch(e->kind){
 	case Ematch:
 		/* syntax in patterns is expanded later */
-		sete2(e, stx(e->e2));
+		sete2(e, stx(ctx, e->e2));
 		return e;
 	case Estx:
 		id = idsym(e->e1);
-		if(!strcmp(id, "id"))
-			return putsrc(Zcall(G("mkstxid"), 1, stx(e->e2)),
+		if(!strcmp(id, "id")){
+			if(elistlen(e->e2) != 1)
+				cerror(ctx, e, "wrong number of arguments to #id");
+			return putsrc(Zcall(G("mkstxid"), 1, stx(ctx, e->e2->e1)),
 				      e->src);
-		else if(!strcmp(id, "val"))
-			return putsrc(Zcall(G("mkstxval"), 1, stx(e->e2)),
+		}else if(!strcmp(id, "val")){
+			if(elistlen(e->e2) != 1)
+				cerror(ctx, e, "wrong number of arguments to #val");
+			return putsrc(Zcall(G("mkstxval"), 1, stx(ctx, e->e2->e1)),
 				      e->src);
-		else
+		}else
 			return putsrc(Zapply(G("mkstx"),
-					     Zcons(Zid2sym(e->e1), stx(e->e2))),
+					     Zcons(Zid2sym(e->e1), stx(ctx, e->e2))),
 				      e->src);
 	case Eelist:
 		p = e;
 		while(p->kind == Eelist){
-			sete1(p, stx(p->e1));
+			sete1(p, stx(ctx, p->e1));
 			p = p->e2;
 		}
 		return e;
 	default:
-		sete1(e, stx(e->e1));
-		sete2(e, stx(e->e2));
-		sete3(e, stx(e->e3));
-		sete4(e, stx(e->e4));
+		sete1(e, stx(ctx, e->e1));
+		sete2(e, stx(ctx, e->e2));
+		sete3(e, stx(ctx, e->e3));
+		sete4(e, stx(ctx, e->e4));
 		return e;
 	}
 }
@@ -48,6 +52,6 @@ docompilex(U *ctx, Expr *e)
 {
 	if(setjmp(ctx->jmp) != 0)
 		return 0;	/* error */
-	e = stx(e);
+	e = stx(ctx, e);
 	return e;
 }
