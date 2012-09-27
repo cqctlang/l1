a2d2ae18a5449ceb1ddcba21224c93d475df83c8
vic zandy <zandy@cs.wisc.edu>
2012-09-26 21:21:21 -0400

    fndlopen.c: reformat

--- fndlopen.c
+++ fndlopen.c
@@ -1,6 +1,3 @@
-
-//provides the ability to dynamically load libraries
-
 #include "sys.h"
 #include "util.h"
 #include "syscqct.h"
@@ -10,92 +7,91 @@
 void
 l1_dlopen(VM *vm, Imm argc, Val *argv, Val *rv)
 {
-    Str *names;
-    char* name;
-    void* handle;
-    int mode;
+	Str *names;
+	char* name;
+	void* handle;
+	int mode;
 
-    if (argc != 2)
-      vmerr(vm, "wrong number of arguments to dlopen");
-    checkarg(vm, argv, 0, Qstr);
-    names = valstr(argv[0]);
-    name = str2cstr(names);
-    mode = (int)valimm(argv[1]);
+	if(argc != 2)
+		vmerr(vm, "wrong number of arguments to dlopen");
+	checkarg(vm, argv, 0, Qstr);
+	names = valstr(argv[0]);
+	name = str2cstr(names);
+	mode = (int)valimm(argv[1]);
 
-    handle = dlopen(name,mode);
+	handle = dlopen(name, mode);
 
-    efree(name);
-    name = 0;
+	efree(name);
+	name = 0;
 
-    *rv = mkvallitcval(Vulong,(Imm)handle);
+	*rv = mkvallitcval(Vulong, (Imm)handle);
 }
 
 void
 l1_dlclose(VM *vm, Imm argc, Val *argv, Val *rv)
 {
-  void* handle;
-  int ret;
+	void* handle;
+	int ret;
 
-  if (argc != 1)
-    vmerr(vm, "wrong number of arguments to libclose");
-  checkarg(vm, argv, 0, Qcval);
-  checkarg(vm, argv, 1, Qstr);
+	if(argc != 1)
+		vmerr(vm, "wrong number of arguments to libclose");
+	checkarg(vm, argv, 0, Qcval);
+	checkarg(vm, argv, 1, Qstr);
 
-  handle = (void*)valimm(argv[0]);
+	handle = (void*)valimm(argv[0]);
 
-  ret = dlclose(handle);
+	ret = dlclose(handle);
 
-  *rv = mkvallitcval(Vint,ret);
+	*rv = mkvallitcval(Vint, ret);
 }
 
 void
 l1_dlsym(VM *vm, Imm argc, Val *argv, Val *rv)
 {
-  void* handle;
-  void* addr;
-  Str *ftnnm;
-  char *ftn;
+	void* handle;
+	void* addr;
+	Str *ftnnm;
+	char *ftn;
 
-  if (argc != 2)
-    vmerr(vm, "wrong number of arguments to ftnaddr");
-  checkarg(vm, argv, 0, Qcval);
-  checkarg(vm, argv, 1, Qstr);
+	if(argc != 2)
+		vmerr(vm, "wrong number of arguments to ftnaddr");
+	checkarg(vm, argv, 0, Qcval);
+	checkarg(vm, argv, 1, Qstr);
 
-  handle = (void*)valimm(argv[0]);
+	handle = (void*)valimm(argv[0]);
 
-  ftnnm = valstr(argv[1]);
-  ftn = str2cstr(ftnnm);
+	ftnnm = valstr(argv[1]);
+	ftn = str2cstr(ftnnm);
 
-  addr = dlsym(handle,ftn);
+	addr = dlsym(handle, ftn);
 
-  efree(ftn);
+	efree(ftn);
 
-  *rv = mkvallitcval(Vulong,(Imm)addr);
+	*rv = mkvallitcval(Vulong, (Imm)addr);
 }
 
 void
 l1_dlerror(VM *vm, Imm argc, Val *argv, Val *rv)
 {
-  char *ret;
+	char *ret;
 
-  if (argc != 0)
-    vmerr(vm, "wrong number of arguments to ftnaddr");
+	if(argc != 0)
+		vmerr(vm, "wrong number of arguments to ftnaddr");
 
-  ret = dlerror();
+	ret = dlerror();
 
-  *rv = mkvalcval(litdom,
-                  mkctypeptr(
-                    mkctypebase(Vchar,
-                                typerep(litdom->ns->base[Vptr])),
-                    typerep(litdom->ns->base[Vptr])),
-                  (uptr)ret);
+	*rv = mkvalcval(litdom,
+			mkctypeptr(mkctypebase(Vchar,
+					       typerep(litdom->ns->base[Vptr])),
+				   typerep(litdom->ns->base[Vptr])),
+			(uptr)ret);
 }
 
 void
 fndlopen(Env *env)
 {
-  FN(dlopen);
-  FN(dlsym);
-  FN(dlclose);
-  FN(dlerror);
+	FN(dlopen);
+	FN(dlsym);
+	FN(dlclose);
+	FN(dlerror);
 }
