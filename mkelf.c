6070861695e8641c46938c54858b56657a63a66d
vic zandy <vic@u>
2013-02-28 13:27:59 -0500

    mkelf: introduce -m option

--- mkelf.c
+++ mkelf.c
@@ -36,11 +36,18 @@ xwrite(int fd, void *p, uptr len)
 }
 
 typedef
-struct Symtab
+struct Symtab64
 {
 	Elf64_Sym tab[Maxsymtab];
 	Elf64_Sym *p, *e;
-} Symtab;
+} Symtab64;
+
+typedef
+struct Symtab32
+{
+	Elf32_Sym tab[Maxsymtab];
+	Elf32_Sym *p, *e;
+} Symtab32;
 
 typedef
 struct Strtab
@@ -49,16 +56,45 @@ struct Strtab
 	char *p, *e;
 } Strtab;
 
-static void
-initsymtab(Symtab *st)
+typedef
+struct Sys
 {
-	memset(st, 0, sizeof(Symtab));
+	char *machid;
+	unsigned width;
+	unsigned machine;
+} Sys;
+
+static Sys systems[] =
+{
+	{ "i386",   ELFCLASS32, EM_386 },
+	{ "amd64",  ELFCLASS64, EM_X86_64 },
+	{ "x86_64", ELFCLASS64, EM_X86_64 },
+	{ 0, 0, 0 }
+};
+
+static Sys*
+looksys(char *machid)
+{
+	unsigned i;
+	Sys *s;
+	for(i = 0, s = systems;
+	    i < sizeof(systems)/sizeof(systems[0]);
+	    i++, s++)
+		if(!strcmp(machid, s->machid))
+			return s;
+	return 0;
+}
+
+static void
+initsymtab64(Symtab64 *st)
+{
+	memset(st, 0, sizeof(Symtab64));
 	st->p = st->tab;
 	st->e = st->p+sizeof(st->tab)/sizeof(st->tab[0]);
 }
 
 static void
-addsymtab(Symtab *st,
+addsymtab64(Symtab64 *st,
 	  Elf64_Word name,
 	  Elf64_Section shndx,
 	  Elf64_Addr addr,
@@ -79,7 +115,42 @@ addsymtab(Symtab *st,
 }
 
 static unsigned int
-symtablen(Symtab *st)
+symtab32len(Symtab32 *st)
+{
+	return (st->p-st->tab)*sizeof(st->tab[0]);
+}
+
+static void
+initsymtab32(Symtab32 *st)
+{
+	memset(st, 0, sizeof(Symtab32));
+	st->p = st->tab;
+	st->e = st->p+sizeof(st->tab)/sizeof(st->tab[0]);
+}
+
+static void
+addsymtab32(Symtab32 *st,
+	  Elf32_Word name,
+	  Elf32_Section shndx,
+	  Elf32_Addr addr,
+	  Elf32_Addr size)
+{
+	Elf32_Sym *s;
+	if(st->p >= st->e){
+		fprintf(stderr, "symbol table overflow\n");
+		exit(1);
+	}
+	s = st->p++;
+	s->st_name = name;
+	s->st_info = ELF32_ST_INFO(STB_GLOBAL, STT_OBJECT);
+	s->st_other = ELF32_ST_VISIBILITY(STV_DEFAULT);
+	s->st_shndx = shndx;
+	s->st_value = addr;
+	s->st_size = size;
+}
+
+static unsigned int
+symtab64len(Symtab64 *st)
 {
 	return (st->p-st->tab)*sizeof(st->tab[0]);
 }
@@ -114,31 +185,31 @@ strtablen(Strtab *st)
 }
 
 static void
-mkelfo(int fd, char *sym, void *data, uptr len, uptr align)
+mkelfo64(Sys *sys, int fd, char *sym, void *data, uptr len, uptr align)
 {
 	Elf64_Ehdr hdr;
 	Elf64_Shdr shdr[Nsect];	
 	Strtab shstrtab;
 	Strtab symstrtab;
-	Symtab symtab;
+	Symtab64 symtab;
 	uptr dataoff, shstrtaboff, symtaboff, symstrtaboff;
 	char endsym[128];
 
 	initstrtab(&shstrtab);
 	initstrtab(&symstrtab);
-	initsymtab(&symtab);
+	initsymtab64(&symtab);
 
 	memset(&hdr, 0, sizeof(hdr));
 	hdr.e_ident[EI_MAG0] = ELFMAG0;
 	hdr.e_ident[EI_MAG1] = ELFMAG1;
 	hdr.e_ident[EI_MAG2] = ELFMAG2;
 	hdr.e_ident[EI_MAG3] = ELFMAG3;
-	hdr.e_ident[EI_CLASS] = ELFCLASS64;
+	hdr.e_ident[EI_CLASS] = sys->width;
 	hdr.e_ident[EI_DATA] = ELFDATA2LSB;
 	hdr.e_ident[EI_VERSION] = EV_CURRENT;
 	hdr.e_ident[EI_OSABI] = ELFOSABI_SYSV;
 	hdr.e_type = ET_REL;
-	hdr.e_machine = EM_X86_64;
+	hdr.e_machine = sys->machine;
 	hdr.e_version = EV_CURRENT;
 	hdr.e_entry = 0;
 	hdr.e_phoff = 0;
@@ -198,17 +269,17 @@ mkelfo(int fd, char *sym, void *data, uptr len, uptr align)
 	shdr[4].sh_addralign = 8;
 	shdr[4].sh_entsize = sizeof(Elf64_Sym);
 
-	addsymtab(&symtab,
-		  addstrtab(&symstrtab, sym),
-		  2,
-		  0,
-		  len);
+	addsymtab64(&symtab,
+		    addstrtab(&symstrtab, sym),
+		    2,
+		    0,
+		    len);
 	snprintf(endsym, sizeof(endsym), "end%s", sym);
-	addsymtab(&symtab,
-		  addstrtab(&symstrtab, endsym),
-		  2,
-		  len,
-		  0);
+	addsymtab64(&symtab,
+		    addstrtab(&symstrtab, endsym),
+		    2,
+		    len,
+		    0);
 
 	shstrtaboff = sizeof(Elf64_Ehdr)+Nsect*sizeof(Elf64_Shdr);
 	dataoff = shstrtaboff+strtablen(&shstrtab);
@@ -225,20 +296,25 @@ mkelfo(int fd, char *sym, void *data, uptr len, uptr align)
 	shdr[3].sh_size = strtablen(&symstrtab);
 
 	shdr[4].sh_offset = symtaboff;
-	shdr[4].sh_size = symtablen(&symtab);
+	shdr[4].sh_size = symtab64len(&symtab);
 
 	xwrite(fd, &hdr, sizeof(Elf64_Ehdr));
 	xwrite(fd, shdr, Nsect*sizeof(Elf64_Shdr));
 	xwrite(fd, shstrtab.tab, strtablen(&shstrtab));
 	xwrite(fd, data, len);
 	xwrite(fd, symstrtab.tab, strtablen(&symstrtab));
-	xwrite(fd, symtab.tab, symtablen(&symtab));
+	xwrite(fd, symtab.tab, symtab64len(&symtab));
+}
+
+static void
+mkelfo32(Sys *sys, int fd, char *sym, void *data, uptr len, uptr align)
+{
 }
 
 static void
 usage(char *argv0)
 {
-	printf("usage: %s -a <alignment> -s <symbol> [ -l <maxlen> ] <infile> <outfile>\n",
+	printf("usage: %s -a <alignment> -s <symbol> -m <arch> [ -l <maxlen> ] <infile> <outfile>\n",
 	       argv0);
 	exit(1);
 }
@@ -253,13 +329,16 @@ main(int argc, char *argv[])
 	char *sym;
 	void *data;
 	char *ep;
+	char *mach;
 	char *infile, *outfile;
+	Sys *sys;
 
 	maxlen = 0;
 	align = 0;
 	len = 0;
 	sym = 0;
-	while(-1 != (c = getopt(argc, argv, "a:hl:s:")))
+	mach = 0;
+	while(-1 != (c = getopt(argc, argv, "a:hl:m:s:")))
 		switch(c){
 		case 'a':
 			opt['a'] = 1;
@@ -277,13 +356,17 @@ main(int argc, char *argv[])
 			if(*ep != '\0')
 				usage(argv[0]);
 			break;
+		case 'm':
+			opt['m'] = 1;
+			mach = optarg;
+			break;
 		case 'h':
 		case '?':
 			usage(argv[0]);
 			break;
 		}
 
-	if(opt['a'] == 0 || opt['s'] == 0)
+	if(opt['a'] == 0 || opt['s'] == 0 || opt['m'] == 0)
 		usage(argv[0]);
 
 	if(argc-optind != 2)
@@ -291,6 +374,12 @@ main(int argc, char *argv[])
 
 	infile = argv[optind];
 	outfile = argv[optind+1];
+
+	sys = looksys(mach);
+	if(sys == 0){
+		printf("%s: unsupported architecture: %s\n", argv[0], mach);
+		exit(1);
+	}
 
 	ifd = open(infile, O_RDONLY);
 	if(0 > ifd){
@@ -318,7 +407,10 @@ main(int argc, char *argv[])
 		fprintf(stderr, "open: %s: %s\n", outfile, strerror(errno));
 		exit(1);
 	}
-	mkelfo(ofd, sym, data, len, align);
+	if(sys->width == ELFCLASS64)
+		mkelfo64(sys, ofd, sym, data, len, align);
+	else
+		mkelfo32(sys, ofd, sym, data, len, align);
 	munmap(data, mlen);
 	close(ofd);
 	return 0;
