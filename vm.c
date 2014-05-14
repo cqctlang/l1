cqctlang/l1

commit 3f5701988593c5e1fc7751f289390d87f16e6da2

fix bug with H and gotab preventing 2nd cqctinit

vm.c
@@ -4375,7 +4375,7 @@ dovm(VM *vm)
 	Val v1, v2, rv;
 
 #ifdef THREADED
-	if(!gotab[0]) {
+	if(!vm) {
 		/* we've been called by initvm.
 		   we're only here to initialize gotab. */
 		gotab[Iabort] 	= &&Iabort;

mem.c
@@ -2259,7 +2259,7 @@ void
 initmem()
 {
 	u32 i, gr;
-
+	memset(&H, 0, sizeof(H));
 	initsegmap();
 	gr = 1;
 	for(i = 0; i < Ngen; i++){

commit a70ecfc51aedb76e5c899042f8bcde1a0dac154f

fix bug introduced by 3f5701988593c5e1
- see original fix for gcc 4.7/8 which 3f5701988593c5e1 reverted:
   aea90d2982d7a165
- the gcc bug is that constant propagation causes dovm(0) to invoke a
clone of dovm. In the clone, computed gotos are incorrect, and gotab
is initialized with junk.
- this fix sets a function attribute to prevent cloning of dovm

vm.c
@@ -4367,6 +4367,7 @@ dogc(VM *vm, u32 g, u32 tg)
 }
 
 Val
+__attribute__ ((noclone))
 dovm(VM *vm)
 {
 	Insn *i;