cqctlang/l1

commit c4689036e0d7c4296b6f919e9d35711eff72d77e

fix xfd saving to heap, io redirection, memory globals.

vm.c
@@ -7591,6 +7591,12 @@ mktopenv(void)
 	/* expanded source may call these magic functions */
 	builtinfn(env, "$put", mkcfn("$put", l1_put));
 	builtinfn(env, "$typeof", mkcfn("$typeof", l1_typeof));
+	builtinfd(env, "stdin",
+		  mkfdfn(mkstr0("<stdin>"), Fread, &l1stdin));
+	builtinfd(env, "stdout",
+		  mkfdfn(mkstr0("<stdout>"), Fwrite, &l1stdout));
+	builtinfd(env, "stderr",
+		  mkfdfn(mkstr0("<stderr>"), Fwrite, &l1stderr));
 
 	return env;
 }

lib/io.cqct
@@ -2,7 +2,7 @@
 	fdopen,
 	fprintf,
 	ioctl,
-	mapfile,      
+	mapfile,
 	open,
 	close,
 	popen,
@@ -268,9 +268,9 @@ ions = @names c32le {
 	_close(fd);
 }
 
-
-stdin = mksysfdfinal(0, ions`Fread, "(stdin)");
-stdout = mksysfdfinal(1, ions`Fwrite, "(stdout)");
-stderr = mksysfdfinal(2, ions`Fwrite, "(stderr)");
+// Now created in vm.c
+//stdin = mksysfdfinal(0, ions`Fread, "(stdin)");
+//stdout = mksysfdfinal(1, ions`Fwrite, "(stdout)");
+//stderr = mksysfdfinal(2, ions`Fwrite, "(stderr)");
 
 }

mem.c
@@ -2260,6 +2260,7 @@ initmem()
 {
 	u32 i, gr;
 	memset(&H, 0, sizeof(H));
+	ncfn = 0;
 	initsegmap();
 	gr = 1;
 	for(i = 0; i < Ngen; i++){
@@ -2563,6 +2564,7 @@ loadsaveptr(Val *pp, LSctx *ls)
 static void
 savecfn(void **fnp)
 {
+    if (*fnp)
 	*fnp = (void*)mkreloc(Segcfn, lookcfnaddr(*fnp));
 }
 

unix.c
@@ -1,5 +1,6 @@
 #include "sys.h"
 #include "util.h"
+#include "syscqct.h"
 
 char sysarch[256];
 char sysos[256];
@@ -69,8 +70,7 @@ xwrite(int fd, void *p, Imm len)
 void
 xvprintf(char *fmt, va_list args)
 {
-	vfprintf(stdout, fmt, args);
-	fflush(stdout);
+	cvprintf(&l1stdout, fmt, args);
 }
 
 void

commit 14e7ae4a6113ffeb4a50d9067758eb4876bfcd44

fix bug introduced by c4689036e0d7c429
- by setting ncfn to 0 in initmem, the addresses for
xfd[close|read|write] were clobbered.

vm.c
@@ -7685,9 +7685,9 @@ cqctinitxfd(char *memfile, char **loadpath, Xfd *in, Xfd *out, Xfd *err)
 		err->fd = 2;
 	}
 
-	initio(in, out, err);
 	initos();
 	initmem();
+	initio(in, out, err);
 	initqc();
 	initparse();
 	initcid();