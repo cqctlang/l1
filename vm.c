754165f1a351a8e4b0843717bc1aedc11760048d
Dan Ridge <dskr@mac.com>
2014-12-02 09:18:42 -0500

    removed builtin stdin, out, err

--- vm.c
+++ vm.c
@@ -7625,12 +7625,6 @@ mktopenv(void)
 	/* expanded source may call these magic functions */
 	builtinfn(env, "$put", mkcfn("$put", l1_put));
 	builtinfn(env, "$typeof", mkcfn("$typeof", l1_typeof));
-	builtinfd(env, "stdin",
-		  mkfdfn(mkstr0("<stdin>"), Fread, &l1stdin));
-	builtinfd(env, "stdout",
-		  mkfdfn(mkstr0("<stdout>"), Fwrite, &l1stdout));
-	builtinfd(env, "stderr",
-		  mkfdfn(mkstr0("<stderr>"), Fwrite, &l1stderr));
 
 	return env;
 }
