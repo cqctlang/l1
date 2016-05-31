0cafd93f0cd6a5fd6c017474df736d618a225418
Nate Rosenblum <natr@google.com>
2016-05-31 10:38:24 -0700

    Add history to repl
    Presently fixed at 100 lines.

--- fnrepl.c
+++ fnrepl.c
@@ -34,7 +34,43 @@ l1__prompt(VM *vm, Imm argc, Val *argv, Val *rv)
     }
 }
 
+void
+l1__prompt_history_set_max_len(VM *vm, Imm argc, Val *argv, Val *rv)
+{
+    int ret;
+
+    if (argc != 1)
+        vmerr(vm, "wrong number of arguments to prompt");
+    if (!Viskind(argv[0], Qcval))
+        vmerr(vm, "operand 1 to prompt_history_set_max_len must be a cvalue");
+
+    ret = linenoiseHistorySetMaxLen(cvalu(valcval(argv[0])));
+    *rv = mkvallitcval(Vint, ret);
+}
+
+void
+l1__prompt_history_add(VM *vm, Imm argc, Val *argv, Val *rv)
+{
+    char *cline, *malloc_cline;
+    int ret;
+    if (argc != 1)
+        vmerr(vm, "wrong number of arguments to prompt");
+    if (!Viskind(argv[0], Qstr))
+        vmerr(vm, "operand 1 to prompt_history_add must be a string");
+
+    /* We need something that can be freed by the linenoise library, which
+       doesn't presently allow overriding allocator methods. */
+    cline = str2cstr(valstr(argv[0]));
+    malloc_cline = strdup(cline);
+    efree(cline);
+
+    ret = linenoiseHistoryAdd(malloc_cline);  /* linenoise now owns */
+    *rv = mkvallitcval(Vint, ret);
+}
+
 void fnrepl(Env env)
 {
     FN(_prompt);
+    FN(_prompt_history_set_max_len);
+    FN(_prompt_history_add);
 }
