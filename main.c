ea27d5c3825ea9264bc862b34d21916d611fae30
Nate Rosenblum <natr@google.com>
2017-08-08 08:18:25 -0700

    cleanup: remove dead code

--- main.c
+++ main.c
@@ -328,14 +328,6 @@ main(int argc, char *argv[])
 			argc -= 2;
 		} else
 			break;
-		//} else if(strcmp("--", argv[i]) == 0 || argv[i][0] != '-')
-			//break;
-		//else {
-			///* pass through legacy flags referenced by run time */
-			//if(argv[i][0] == '-' && strlen(argv[i]) == 2)
-				//cqctflags[(int)argv[i][1]] = 1;
-			//i++;
-		//}
 	}
 
 	nlp = finishloadpath(lp, nlp);
